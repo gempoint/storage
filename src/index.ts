de0f9d3bed1bdeb5b0788ba459d482312d862336
TheHackerCoding <triniziah@gmail.com>
2022-12-24 13:23:04 -0500

    interesting start

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+export default class Storage<T> {
+  private dat: Record<string, T> = {}
+
+  delete(key: string) { delete this.dat[key] }
+
+  create(key: string, dat: T) {
+    this.dat[key] = dat;
+  }
+
+  get(key: string): T | undefined { return this.dat[key] }
+
+  filter(dat: (x: T) => boolean) {
+    let result: FilterResults<T> = {
+      count: 0,
+      items: {}
+    }
+    for (const y in this.dat) {
+      const value = this.dat[y];
+      const test = dat(value)
+      if (test) {
+        result.count++
+        result.items[y] = value
+      }
+    }
+    return result;
+  }
+
+  deleteAll() { this.dat = {} }
+}
+
+export class MultiStorage {
+  private dats: Record<string, Storage<unknown>> = {}
+
+  newStorage<T>(key: string) {
+    this.dats[key] = new Storage<T>()
+  }
+
+  access(key: string): Storage<unknown> | undefined { return this.dats[key] }
+
+  delete(key: string) { delete this.dats[key] }
+
+  deleteAll() { this.dats = {} }
+}
+
+export interface FilterResults<T> {
+  count: number
+  items: { [x: string]: T }
+}
